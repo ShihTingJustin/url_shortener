9056421b3c0a12ad6797f2964554b09d7228da05
Justin Huang <justinhuang777@gmail.com>
2020-05-31 00:46:20 +0800

    feat: add url manager function and page

--- routes/modules/manager.js
+++ routes/modules/manager.js
@@ -4,8 +4,12 @@ const request = require('request')
 const URL = require('../../models/urls')
 
 router.get('/', (req, res) => {
-  res.render('manager')
+  return URL.find()
+    .lean()
+    .then(urls => {
+      res.render('manager', { urls })
+    })
+    .catch(error => console.log(error))
 })
-
 
 module.exports = router
--- routes/modules/shorten.js
+++ routes/modules/shorten.js
@@ -5,12 +5,37 @@ const URL = require('../../models/urls')
 // redirect to origin url
 router.get('/:shorten', (req, res) => {
   const { shorten } = req.params
-  URL.findOne({ shorten: shorten })
-    .lean()
-    .then(url => {
-      res.redirect(`${url.origin}`)
+  // redirect first
+  const redirect = function () {
+    return new Promise((resolve, rej) => {
+      URL.findOne({ shorten: shorten })
+        .lean()
+        .then(url => {
+          resolve(url._id)
+          res.redirect(`${url.origin}`)
+        })
+        .catch(error => console.log(error))
     })
-    .catch(error => console.log(error))
+  }
+
+  // update shortened URL click count
+  const updateClickCount = function (id) {
+    return new Promise((res, rej) => {      
+      URL.findById(id)
+        .then(url => {
+          url.click += 1
+          url.save()
+        })
+        .catch(error => console.log(error))
+    })
+  }
+
+  async function updateClickAsyncAwait() {
+    const value = await redirect()
+    const value1 = await updateClickCount(value)
+  }
+
+  updateClickAsyncAwait()
 })
 
 module.exports = router
